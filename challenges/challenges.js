229b37cdc2f794b5f91c3e9d9f0f8fcf53d127d5
Michael Pearson <mipearson@gmail.com>
2018-10-13 15:14:13 +1100

    Move code from pings.js into Challenges class

--- challenges/challenges.js
+++ challenges/challenges.js
@@ -10,6 +10,82 @@ class Challenges {
         this.crossServerStatus = {}
         this.autoSumStatus = {}
     }
+
+    joinChallenge(msg,suffix) {
+        var challengeID = suffix;
+        if (isNaN(challengeID)) {
+            msg.channel.send("Challenge ID must be an integer.");
+        } else if (challengeID < 1) {
+            msg.channel.send("Challenge ID must be an integer.");
+        } else if (challengeID in challenges.challengeList) {
+            if (challenges.challengeList[challengeID].hidden && challenges
+                .challengeList[challengeID].channelID != msg.channel.id) {
+                msg.channel.send(msg.author + ", you do not have permission"
+                    + " to join this challenge.");
+            } else {
+                if(msg.author.id in challenges.challengeList[challengeID]
+                    .joinedUsers) {
+                    msg.channel.send(msg.author + ", you already have"
+                    + " notifications enabled for this challenge.");
+                } else {
+                    challenges.challengeList[challengeID].joinedUsers
+                        [msg.author.id] = {"countData": undefined,
+                        "countType": undefined, "channelID": msg.channel.id};
+                    var pushID = msg.channel.id;
+                    var searchIndex = challenges.challengeList[challengeID]
+                        .hookedChannels.indexOf(pushID);
+                    if (searchIndex == -1) {
+                        challenges.challengeList[challengeID].hookedChannels
+                            .push(pushID);
+                    }
+                    msg.channel.send(msg.author + ", you have joined "
+                        + challenges.challengeList[challengeID].displayName);
+                    try {
+                        conn.collection("challengeDB").update(
+                            {_id: parseInt(challengeID)},
+                            {$set: {"joinedUsers": challenges.challengeList
+                                [challengeID].joinedUsers}},
+                            {upsert: true}
+                        )
+                    } catch(e) {
+                        logger.info("Error: " + e);
+                    }
+                }
+            }
+        } else {
+            msg.channel.send("Challenge " + challengeID
+                + " does not exist!");
+        }
+    }
+    
+    leaveChallenge(client,msg,suffix) {
+        var challengeID = suffix;
+        if (isNaN(challengeID)) {
+            msg.channel.send("Challenge ID must be an integer.");
+        } else if (challengeID < 1) {
+            msg.channel.send("Challenge ID must be an integer.");
+        } else if (challengeID in challenges.challengeList) {
+            if(msg.author.id in challenges.challengeList[challengeID]
+                .joinedUsers) {
+                delete challenges.challengeList[challengeID]
+                    .joinedUsers[msg.author.id];
+                msg.channel.send(msg.author + ", you have left "
+                    + challenges.challengeList[challengeID].displayName);
+                conn.collection("challengeDB").update(
+                    {_id: parseInt(challengeID)},
+                    {$set: {"joinedUsers": challenges.challengeList
+                        [challengeID].joinedUsers}},
+                    {upsert: OES_texture_half_float}
+                )
+            } else {
+                msg.channel.send(msg.author
+                    + ", you have not yet joined this challenge.");
+            }
+        } else {
+            msg.channel.send("Challenge " + challengeID
+                + " does not exist!");
+        }
+    }
 }
 
 module.exports = new Challenges();
