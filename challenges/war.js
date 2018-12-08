268e46212c5a0d9b1fbb7184c276ae26d9899d66
Dawn E. Collett <lisushka@users.noreply.github.com>
2018-12-08 21:21:14 +1100

    Added lifetime war and sprint totals to user summary. (#55)

--- challenges/war.js
+++ challenges/war.js
@@ -1,4 +1,5 @@
 const Challenge = require('./challenge.js');
+const challengelist = require('./challengelist.js');
 const conn = require('mongoose').connection;
 
 /** Represents a war. */
@@ -75,7 +76,70 @@ class War extends Challenge {
   }
   /** Check to see whether the total period is over, and post the summary. */
   terminate() {
-    super.terminate();
+    this.cPost--;
+    if (this.cPost == 0) {
+      for (const user in this.joinedUsers) {
+        if (this.joinedUsers[user].countType == 'words') {
+          conn.collection('userDB').update(
+              {_id: user},
+              {
+                $inc: {
+                  lifetimeWarWords:
+                  parseInt(this.joinedUsers[user].countData),
+                  lifetimeWordMinutes:
+                  parseFloat(this.duration),
+                },
+              },
+              {upsert: true}
+          );
+        } else if (this.joinedUsers[user].countType == 'lines') {
+          conn.collection('userDB').update(
+              {_id: user},
+              {
+                $inc: {
+                  lifetimeWarLines:
+                  parseInt(this.joinedUsers[user].countData),
+                  lifetimeLineMinutes:
+                  parseFloat(this.duration),
+                },
+              },
+              {upsert: true}
+          );
+        } else if (this.joinedUsers[user].countType == 'pages') {
+          conn.collection('userDB').update(
+              {_id: user},
+              {
+                $inc: {
+                  lifetimeWarPages:
+                  parseInt(this.joinedUsers[user].countData),
+                  lifetimePageMinutes:
+                  parseFloat(this.duration),
+                },
+              },
+              {upsert: true}
+          );
+        } else {
+          conn.collection('userDB').update(
+              {_id: user},
+              {
+                $inc: {
+                  lifetimeWarMinutes:
+                  parseInt(this.joinedUsers[user].countData),
+                },
+              },
+              {upsert: true}
+          );
+        }
+      }
+      for (let i = 0; i < this.hookedChannels.length; i++) {
+        challengelist.generateSummary(
+            client.channels.get(this.hookedChannels[i]),
+            this.objectID
+        );
+      }
+      conn.collection('challengeDB').remove({_id: this.objectID});
+      delete challengelist.challengeList[this.objectID];
+    }
   }
 }
 
