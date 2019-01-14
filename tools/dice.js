f4d795de36d8648e27150adcfc918566116c6d0b
Dawn E. Collett <lisushka@users.noreply.github.com>
2019-01-14 23:08:56 +1100

    Split dice commands into separate methods.

--- tools/dice.js
+++ tools/dice.js
@@ -14,90 +14,21 @@ class Dice {
     let diceSum = 0;
     const faces = suffix.split('+');
     for (let i = 0; i < faces.length; i++) {
+      let diceArray = [];
       if (Number.isInteger(Number(faces[i]))) {
-        // Single number
-        if (faces.length == 1) {
-          // Treat as a 1dx roll
-          const roll = Math.floor(Math.random() * Number(faces[i])) + 1;
-          diceString += roll;
-          diceSum += roll;
-        } else {
-          // Add to the other rolls
-          diceString += '(' + Number(faces[i]) + ')';
-          diceSum += Number(faces[i]);
-        }
+        diceArray = this.flatRoll(faces, i);
       } else if (faces[i].split('d').length == 2) {
-        // RPG-style roll
-        const rpgRoll = faces[i].split('d');
-        if (rpgRoll[0] == '') {
-          rpgRoll[0] = 1;
-        }
-        if (
-          !Number.isInteger(Number(rpgRoll[0])) ||
-          !Number.isInteger(Number(rpgRoll[1]))
-        ) {
-          diceString =
-            '**Error:**: Both values in an RPG-style roll must be integers.' +
-            ' Example: `' +
-            prefix +
-            'roll 2d6`.';
-          diceSum = 0;
-          break;
-        } else {
-          if (rpgRoll[0] > 20) {
-            diceString = '**Error:**: I cannot roll more than 20 dice at once.';
-            diceSum = 0;
-            break;
-          } else {
-            for (let j = 0; j < Number(rpgRoll[0]); j++) {
-              const roll = Math.floor(Math.random() * Number(rpgRoll[1])) + 1;
-              diceString += roll;
-              if (j < Number(rpgRoll[0]) - 1) {
-                diceString += ', ';
-              }
-              diceSum += roll;
-            }
-          }
-        }
+        diceArray = this.rpgRoll(prefix, faces[i]);
       } else if (faces[i].split(' ').length == 2) {
-        // Range roll
-        const rangeRoll = faces[i].split(' ');
-        if (
-          !Number.isInteger(Number(rangeRoll[0])) ||
-          !Number.isInteger(Number(rangeRoll[1]))
-        ) {
-          diceString = '**Error:**: Both values in a range roll must be' +
-            ' integers. Example: `' +
-            prefix +
-            'roll 1 100`.';
-          diceSum = 0;
-          break;
-        } else {
-          if (Number(rangeRoll[0]) < Number(rangeRoll[1])) {
-            const roll = Math.floor(
-                Math.random() *
-                (1 + Number(rangeRoll[1]) - Number(rangeRoll[0])) +
-                Number(rangeRoll[0])
-            );
-            diceString += roll;
-            diceSum += roll;
-          } else {
-            // First number is larger than second
-            diceString =
-              '**Error:**: The first number in a range' +
-              ' roll must be smaller than the second.';
-            diceSum = 0;
-            break;
-          }
-        }
+        diceArray = this.rangeRoll(prefix, faces[i]);
       } else {
         diceString = '**Error:**: ' + faces[i] + ' is not a valid roll.' +
-          ' Example: `' +
-          prefix +
-          'roll 2d6 + 5`.';
+          ' Example: `' + prefix + 'roll 2d6 + 5`.';
         diceSum = 0;
         break;
       }
+      diceString += diceArray[0];
+      diceSum += diceArray[1];
       if (i < faces.length - 1) {
         diceString += ', ';
       }
@@ -106,6 +37,99 @@ class Dice {
       diceString += '\nTotal = ' + diceSum;
     }
     return diceString;
+  }
+  /**
+   * Roll a single dice.
+   * @param {String} faces - The user's roll.
+   * @param {String} i - The section of the string to roll.
+   * @return {Array} - Roll information.
+   */
+  flatRoll(faces, i) {
+    // Single number
+    let diceString = '';
+    let diceSum = 0;
+    if (faces.length == 1) {
+      // Treat as a 1dx roll
+      const roll = Math.floor(Math.random() * Number(faces[i])) + 1;
+      diceString += roll;
+      diceSum += roll;
+    } else {
+      // Add to the other rolls
+      diceString += '(' + faces[i] + ')';
+      diceSum += Number(faces[i]);
+    }
+    return [diceString, diceSum];
+  }
+  /**
+   * Roll dice according to tabeltop RPG specs.
+   * @param {String} prefix - The bot's prefix.
+   * @param {String} data - Dice string.
+   * @return {Array} - Roll information.
+   */
+  rpgRoll(prefix, data) {
+    // RPG-style roll
+    let diceString = '';
+    let diceSum = 0;
+    const rpgRoll = data.split('d');
+    if (rpgRoll[0] == '') {
+      rpgRoll[0] = 1;
+    }
+    if (
+      !Number.isInteger(Number(rpgRoll[0])) ||
+      !Number.isInteger(Number(rpgRoll[1]))
+    ) {
+      diceString =
+        '**Error:**: Both values in an RPG-style roll must be integers.' +
+        ' Example: `' + prefix + 'roll 2d6`.';
+      diceSum = 0;
+    } else if (rpgRoll[0] > 20) {
+      diceString = '**Error:**: I cannot roll more than 20 dice at once.';
+      diceSum = 0;
+    } else {
+      for (let i = 0; i < Number(rpgRoll[0]); i++) {
+        const roll = Math.floor(Math.random() * Number(rpgRoll[1])) + 1;
+        diceString += roll;
+        if (i < Number(rpgRoll[0]) - 1) {
+          diceString += ', ';
+        }
+        diceSum += roll;
+      }
+    }
+    return [diceString, diceSum];
+  }
+  /**
+   * Roll dice according to the user's specifications.
+   * @param {String} prefix - The bot's prefix.
+   * @param {String} data - Information after the bot command.
+   * @return {String} - The message to send to the user.
+   */
+  rangeRoll(prefix, data) {
+    let diceString = '';
+    let diceSum = 0;
+    // Range roll
+    const rangeRoll = data.split(' ');
+    if (
+      !Number.isInteger(Number(rangeRoll[0])) ||
+      !Number.isInteger(Number(rangeRoll[1]))
+    ) {
+      diceString = '**Error:**: Both values in a range roll must be' +
+        ' integers. Example: `' + prefix + 'roll 1 100`.';
+      diceSum = 0;
+    } else if (Number(rangeRoll[0]) < Number(rangeRoll[1])) {
+      const roll = Math.floor(
+          Math.random() *
+          (1 + Number(rangeRoll[1]) - Number(rangeRoll[0])) +
+          Number(rangeRoll[0])
+      );
+      diceString += roll;
+      diceSum += roll;
+    } else {
+      // First number is larger than second
+      diceString = '**Error:**: The first number in a range' +
+        ' roll must be smaller than the second.';
+      diceSum = 0;
+    }
+    return [diceString, diceSum];
   }
 }
 
