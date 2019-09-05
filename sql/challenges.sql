5007e7c50bac3349070ace52a8d3766bb47cb99b
Dawn E. Collett <lisushka@users.noreply.github.com>
2019-09-05 11:33:01 +1000

    Merge phase 1 of Postgres database implementation. (#86)
    * Implemented stage 1 of postgres databse migration.
    
    * Amended docker-compose file to add environment variables.

---
+++ sql/challenges.sql
@@ -0,0 +1,13 @@
+CREATE TABLE challenges(
+    challenge_id    SERIAL      PRIMARY KEY NOT NULL,
+	user_id         VARCHAR(30) NOT NULL,
+	start_time
+	countdown
+	duration
+	channel
+	hookedChannels
+	joined
+	challenge_state
+	challenge_type
+	hidden
+);
