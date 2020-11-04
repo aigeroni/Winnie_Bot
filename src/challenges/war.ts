d0629b9e9b50785821e79582d28f7258bebac587
Katie Macke <katie@sleepysheepy.net>
2020-11-03 21:16:44 -0500

    typescriptify war.ts

--- src/challenges/war.ts
+++ src/challenges/war.ts
@@ -1,40 +1,44 @@
-const Challenge = require('./challenge.js')
-const dbc = require('../dbc.js')
+import Challenge from './challenge'
+import ChallengeStates from './challenge-states'
+import ChallengeTypes from './challenge-types'
+import { ChallengeUser, WarUser } from './challenge-user'
+import { Client, Guild, GuildChannel, Snowflake } from 'discord.js'
 
-/** Represents a war. */
-class War extends Challenge {
+// const dbc = require('../dbc.js')
+
+declare const client: Client
+
+/**
+ * Represents a war.
+ */
+export default class War extends Challenge {
   /**
    * Create a chain war.
-   * @param {Number} objectID - The unique ID of the war.
-   * @param {Number} creator - The Discord ID of the creator.
-   * @param {String} displayName - The name of the war.
-   * @param {Number} initStamp - UNIX timestamp of creation time.
-   * @param {Number} countdown - Time in minutes from creation to start.
-   * @param {Number} duration - Duration in minutes.
-   * @param {String} channel - Discord ID of start channel.
-   * @param {Boolean} hidden - Flag for whether challenge is visible to users
-   *  on other servers.
-   * @param {Array} hookedChannels - A list of channels that have joined the
-   *  war.
-   * @param {Object} joined - A list of users who have joined the war.
-   * @param {String} type - The type of the war.
+   * @param objectID - The unique ID of the war.
+   * @param creator - The Discord ID of the creator.
+   * @param displayName - The name of the war.
+   * @param initStamp - UNIX timestamp of creation time.
+   * @param countdown - Time in minutes from creation to start.
+   * @param duration - Duration in minutes.
+   * @param channel - Discord ID of start channel.
+   * @param hidden - Flag for whether challenge is visible to users on other servers.
+   * @param hookedChannels - A list of channels that have joined the war.
+   * @param joined - A list of users who have joined the war.
+   * @param type - The type of the war.
    */
   constructor(
-    objectID,
-    creator,
-    displayName,
-    initStamp,
-    countdown,
-    duration,
-    channel,
-    hidden,
-    hookedChannels,
-    joined,
-    type,
+    objectID: number,
+    creator: Snowflake,
+    displayName: string,
+    initStamp: number,
+    countdown: number,
+    duration: number,
+    channel: Snowflake,
+    hidden: boolean,
+    hookedChannels: Array<Snowflake>,
+    joined: Record<Snowflake, ChallengeUser>,
+    type = ChallengeTypes.WAR,
   ) {
-    if (type === undefined) {
-      type = 'war'
-    }
     super(
       objectID,
       creator,
@@ -56,11 +60,11 @@ class War extends Challenge {
       startTime: this.initStamp,
       countdown: this.countdown,
       duration: this.duration,
-      channel: this.channelID,
+      channel: this.getChannel(channel),
       hookedChannels: this.hookedChannels,
       joined: this.joined,
       state: this.state,
-      type: 'war',
+      type: ChallengeTypes.WAR,
       hidden: this.hidden,
     }
     const array = [challengeData]
@@ -70,12 +74,13 @@ class War extends Challenge {
 
   /**
    * Builds user data for the challenge database.
-   * @param {String} channel - The channel from which the user joined.
-   * @param {String} total - The total posted by the user.
-   * @param {String} type - The type of the total.
-   * @return {Object} - JSON object representing the total.
+   *
+   * @param channel - The channel from which the user joined.
+   * @param total - The total posted by the user.
+   * @param type - The type of the total.
+   * @return JSON object representing the total.
    */
-  buildUserData(channel, total, type) {
+  buildUserData(channel: Snowflake, total: number, type: string): WarUser {
     return {
       countData: total,
       countType: type,
@@ -83,162 +88,185 @@ class War extends Challenge {
     }
   }
 
-  /** Check to see whether the total period is over, and post the summary. */
-  terminate() {
+  /**
+   * Check to see whether the total period is over, and post the summary.
+   */
+  async terminate(): Promise<void> {
     this.cPost--
-    if (this.cPost <= 0) {
-      for (const user in this.joined) {
-        if (this.joined.hasOwnProperty(user)) {
-          const data = {$inc: {}}
-          if (this.joined[user].countType === 'words') {
-            data.$inc.lifetimeWarWords = parseInt(this.joined[user].countData)
-            data.$inc.lifetimeWordMinutes = parseFloat(this.duration)
-          } else if (this.joined[user].countType === 'lines') {
-            data.$inc.lifetimeWarLines = parseInt(this.joined[user].countData)
-            data.$inc.lifetimeLineMinutes = parseFloat(this.duration)
-          } else if (this.joined[user].countType === 'pages') {
-            data.$inc.lifetimeWarPages = parseInt(this.joined[user].countData)
-            data.$inc.lifetimePageMinutes = parseFloat(this.duration)
-          } else if (this.joined[user].countType === 'minutes') {
-            data.$inc.lifetimeWarMinutes =
-              parseInt(this.joined[user].countData)
-          }
-          dbc.dbUpdate('userDB', {_id: user}, data)
-        }
+    if (this.cPost > 0) { return }
+
+    Object.entries(this.joined).forEach(async ([userID, joinedUser]) => {
+      const warUser = joinedUser as WarUser
+      const data = {
+        $inc: {},
       }
-      super.terminate()
-    }
+
+      switch (warUser.countType) {
+      case 'words':
+        data.$inc = {
+          lifetimeWarWords: warUser.countData,
+          lifetimeWordMinutes: this.duration,
+        }
+        break
+      case 'lines':
+        data.$inc = {
+          lifetimeWarLines: warUser.countData,
+          lifetimeWordMinutes: this.duration,
+        }
+        break
+      case 'pages':
+        data.$inc = {
+          lifetimeWarPages: warUser.countData,
+          lifetimeWordMinutes: this.duration,
+        }
+        break
+      case 'minutes':
+        data.$inc = {
+          lifetimeWordMinutes: warUser.countData,
+        }
+        break
+      }
+
+      await dbc.dbUpdate('userDB', { _id: userID }, data)
+    })
+
+    super.terminate()
   }
 
-  /** Prints statistics for a challenge.
-   * @param {String} channel - Channel to print to.
-   * @return {String} - Return message.
+  /**
+   * Prints statistics for a challenge.
+   *
+   * @param channel - Channel to print to.
+   * @return Return message.
    */
-  stats(channel) {
-    let returnMsg = ''
-    if (this.state >= 2) {
-      const serverTotals = this.serverTotals(this.objectID)
-      const summaryServer = this.getChannel(channel).guild
-      returnMsg += this.challengeByUser(summaryServer, this.objectID, 'war')
-      if (Object.keys(serverTotals).length > 1) {
-        returnMsg += '\n'
-      }
-      for (const server in serverTotals) {
-        if (serverTotals.hasOwnProperty(server)) {
-          returnMsg += this.serverText(server, serverTotals) + '\n'
-        }
-      }
-    } else {
-      returnMsg = 'This war has not ended yet.'
+  stats(channel: Snowflake): string {
+    if (this.state !== ChallengeStates.ENDED) {
+      return 'This war has not ended yet.'
     }
-    if (returnMsg === '') {
-      returnMsg = 'Nobody has posted a total for this war yet.'
+
+    const serverTotals = this.serverTotals()
+    if (Object.keys(serverTotals).length === 0) {
+      return 'Nobody has posted a total for this war yet.'
     }
-    return super.stats() + returnMsg
+
+    let returnMessage = `***Statistics for ${this.displayName}:***\n\n`
+    returnMessage += `${this.challengeByUser(this.getChannel(channel).guild)}\n`
+
+    for (const server in serverTotals) {
+      returnMessage += this.serverText(server, serverTotals) + '\n'
+    }
+
+    return returnMessage
   }
 
   /**
    * Summarises all totals for a given server.
-   * @param {String} summaryServer - The server being summarised.
-   * @return {String} - The message to send to the user.
+   *
+   * @param summaryServer - The server being summarised.
+   * @return The message to send to the user.
    */
-  challengeByUser(summaryServer) {
+  challengeByUser(summaryServer: Guild): string {
     let userTotals = ''
-    for (const user in this.joined) {
-      if (this.joined.hasOwnProperty(user)) {
-        const cType = this.joined[user].countType
-        if (cType !== undefined && client.channels.get(this.joined[user]
-          .channelID).guild.id === summaryServer.id) {
-          userTotals += client.users.get(user) + ': ' + this.userTotals(
-            this.joined[user].countData, cType, this.duration,
-          ) + '\n'
-        }
+
+    Object.entries(this.joined).forEach(async ([userID, joinedUser]) => {
+      const warUser = joinedUser as WarUser
+      const userChannel = client.channels.get(warUser.channelID) as GuildChannel
+      const userInSummaryServer = userChannel?.guild?.id === summaryServer.id
+
+      if (warUser.countType && userInSummaryServer) {
+        const userTotal = this.userTotals(warUser.countData, warUser.countType, this.duration)
+        userTotals += `${client.users.get(userID)}: ${userTotal}\n`
       }
-    }
+    })
+
     return userTotals
   }
 
   /**
    * Produces a per-server breakdown of user-entered totals for a war.
-   * @return {String} - The message to send to the user.
+   *
+   * @return The message to send to the user.
    */
-  serverTotals() {
-    let serverTotals = {}
-    for (const user in this.joined) {
-      if (this.joined[user].countType !== undefined) {
-        const homeServer =
-          this.getChannel(this.joined[user].channelID).guild.id
-        serverTotals = this.addToAggregate(serverTotals, homeServer)
-        serverTotals[homeServer][this.joined[user].countType][0] +=
-            parseInt(this.joined[user].countData)
-        serverTotals[homeServer][this.joined[user].countType][1] += 1
+  serverTotals(): Record<Snowflake, Record<string, Array<number>>> {
+    let serverTotals: Record<Snowflake, Record<string, Array<number>>> = {}
+
+    Object.entries(this.joined).forEach(async ([, joinedUser]) => {
+      const warUser = joinedUser as WarUser
+
+      if (warUser.countType) {
+        const userServerID = this.getChannel(warUser.channelID).guild.id
+        serverTotals = this.addToAggregate(serverTotals, userServerID)
+
+        serverTotals[userServerID][warUser.countType][0] += warUser.countData
+        serverTotals[userServerID][warUser.countType][1] += 1
       }
-    }
+    })
+
     return serverTotals
   }
 
   /**
    * Adds an object to an aggregate list of totals.
-   * @param {Object} serverList - The object to add to.
-   * @param {String} server - The object to add.
-   * @return {String} - The relevant server.
+   *
+   * @param serverList - The object to add to.
+   * @param server - The object to add.
+   * @return The relevant server.
    */
-  addToAggregate(serverList, server) {
-    if (serverList[server] === undefined) {
-      serverList[server] = {
-        words: [0, 0],
-        lines: [0, 0],
-        pages: [0, 0],
-        minutes: [0, 0],
-      }
+  addToAggregate(serverList: Record<Snowflake, Record<string, Array<number>>>, server: Snowflake): Record<Snowflake, Record<string, Array<number>>> {
+    if (serverList[server] !== undefined) { return serverList }
+
+    serverList[server] = {
+      words: [0, 0],
+      lines: [0, 0],
+      pages: [0, 0],
+      minutes: [0, 0],
     }
+
     return serverList
   }
 
   /**
    * Summarises a user's total.
-   * @param {Number} total - The user's total.
-   * @param {String} type - The total type.
-   * @param {Number} time - The duration of the challenge.
-   * @return {String} - The message to send to the user.
+   *
+   * @param total - The user's total.
+   * @param type - The total type.
+   * @param time - The duration of the challenge.
+   * @return The message to send to the user.
    */
-  userTotals(total, type, time) {
-    let userTotal = ''
-    if (total === 1) {
-      userTotal += '**' + total + '** ' + type.slice(0, -1)
-    } else {
-      userTotal += '**' + total + '** ' + type
-    }
+  userTotals(total: number, type: string, time: number): string {
+    let userTotal = `**${total}** ${total === 1 ? type.slice(0, -1) : type}`
+
     if (type !== 'minutes') {
-      userTotal +=
-        ' (**' + (total/time).toFixed(2) + '** ' + type.slice(0, 1) + 'pm)'
+      userTotal += ` (**${(total/time).toFixed(2)}** ${type.slice(0, 1)}pm)`
     }
+
     return userTotal
   }
 
   /**
    * Displays war totals in a human-readable format.
+   *
    * @param {String} server - Snowflake of the server being posted to.
    * @param {Object} serverTotals - Summary of user-entered totals by server.
    * @return {String} - The message to send to the user.
    */
-  serverText(server, serverTotals) {
-    let serverText = '__' + client.guilds.get(server).name + '__:'
+  serverText(serverID: Snowflake, serverTotals: Record<Snowflake, Record<string, Array<number>>>): string {
+    let serverText = `__${client.guilds.get(serverID)?.name}__:`
     let firstType = true
-    for (const item in serverTotals[server]) {
-      if (serverTotals[server][item][1] > 0) {
+    for (const item in serverTotals[serverID]) {
+      if (serverTotals[serverID][item][1] > 0) {
         if (!firstType) {
           serverText += ', '
         }
-        serverText += ' **' + serverTotals[server][item][0]
-        if (serverTotals[server][item][0] === 1) {
+        serverText += ' **' + serverTotals[serverID][item][0]
+        if (serverTotals[serverID][item][0] === 1) {
           serverText += '** ' + item.slice(0, -1)
         } else {
           serverText += '** ' + item
         }
         serverText += ' (**' + (
-          serverTotals[server][item][0]/
-          serverTotals[server][item][1]).toFixed(0) +
+          serverTotals[serverID][item][0]/
+          serverTotals[serverID][item][1]).toFixed(0) +
           '** avg)'
         firstType = false
       }
@@ -246,5 +274,3 @@ class War extends Challenge {
     return serverText
   }
 }
-
-module.exports = War
