89f9d836f118c2133cb2f2b82f994437282b41a8
Katie Macke <katie@sleepysheepy.net>
2020-12-02 02:03:17 -0500

    handle mentions

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,7 +1,9 @@
 import GuildCreate from './guild-create'
+import MessageEvent from './message'
 import ReadyEvent from './ready'
 
 export default [
   GuildCreate,
+  MessageEvent,
   ReadyEvent,
 ]
---
+++ src/events/message.ts
@@ -0,0 +1,20 @@
+import I18n from '../core/i18n'
+import { Client, Message } from 'discord.js'
+import { Event } from '../core/types'
+import { GuildConfig } from '../models'
+
+const MessageEvent: Event = {
+  name: 'message',
+  handle: async (message: Message, client: Client): Promise<void> => {
+    if (!client.user) { return }
+    if (message.mentions.has(client.user?.id)) { return }
+
+    const guildConfig = await GuildConfig.findOne(message.guild?.id)
+    if (!guildConfig) { return }
+
+    const response = await I18n.translate('en', 'mentionResponse', { prefix: guildConfig.prefix })
+    message.channel.send(response)
+  },
+}
+
+export default MessageEvent
