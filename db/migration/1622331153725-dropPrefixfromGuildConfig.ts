1b02b68ea392272b04baa0da14fdcf2604557015
Katie Macke <katie@sleepysheepy.net>
2021-05-29 19:37:23 -0400

    drop prefix from guild_config table

---
+++ db/migration/1622331153725-dropPrefixfromGuildConfig.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
+
+export class dropPrefixfromGuildConfig1622331153725 implements MigrationInterface {
+  name = 'dropPrefixfromGuildConfig1622331153725'
+
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('guild_config', 'prefix')
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn('guild_config', new TableColumn({
+      name: 'prefix',
+      type: 'varchar',
+      length: '3',
+      isNullable: false
+    }))
+  }
+}
