61cf3056a872cf6c4f3f2393c0820c4e78ed3ff1
lisushka <lisushka@users.noreply.github.com>
2021-08-01 23:58:26 +1000

    Configure database and firewall rules

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,8 +1,23 @@
-resource "digitalocean_database_cluster" "winnie-db" {
+resource "digitalocean_database_cluster" "winnie-db-cluster" {
   name       = "winnie-db"
   engine     = "pg"
   version    = "13"
   size       = "db-s-1vcpu-1gb"
   region     = "ams3"
   node_count = 1
+}
+
+resource "digitalocean_database_db" "winnie-db" {
+  cluster_id = digitalocean_database_cluster.winnie-db-cluster.id
+  name       = "Winnie_DB"
+}
+
+resource "digitalocean_database_firewall" "winnie-db-firewall" {
+  depends_on = [digitalocean_droplet.winnie-bot]
+
+  cluster_id = digitalocean_database_cluster.winnie-db-cluster.id
+  rule {
+    type  = "droplet"
+    value = digitalocean_droplet.winnie-bot.id
+  }
 }
