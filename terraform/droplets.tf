6c82e2d30734eadb23c00e60608b736d5808de11
lisushka <lisushka@users.noreply.github.com>
2021-08-02 00:05:04 +1000

    First cut of droplet firewall rule

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -12,6 +12,18 @@ resource "digitalocean_droplet" "winnie-bot" {
   ]
 }
 
+resource "digitalocean_firewall" "winnie-bot-firewall" {
+  name = "block-port-access"
+
+  droplet_ids = [digitalocean_droplet.winnie-bot.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"] // TODO: we will need to look at limiting this using the Github Actions API
+  }
+}
+
 output "droplet_ip_address" {
   value      = digitalocean_droplet.bot.ipv4_address
 }
