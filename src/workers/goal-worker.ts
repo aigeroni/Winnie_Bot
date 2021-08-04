de558fdf355ea9cc3053bee5425e29da08923dd0
Katie Macke <katie@sleepysheepy.dev>
2021-08-04 01:14:37 -0400

    Create a worker for goals queue

---
+++ src/workers/goal-worker.ts
@@ -0,0 +1,10 @@
+import { BaseWorker } from './base-worker'
+import { JobQueue } from '../core'
+import { Jobs } from '../jobs'
+import { createConnection } from 'typeorm'
+
+createConnection().then(() => {
+  const worker = new BaseWorker(JobQueue.queueNames.goals, Jobs.goalJobs)
+  process.on('SIGTERM', () => worker.gracefulShutdown())
+  process.on('SIGINT', () => worker.gracefulShutdown())
+}).catch(() => {})
