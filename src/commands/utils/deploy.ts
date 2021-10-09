dcfd172fe479495c24629400952933a37cbd7c04
Katie Macke <katie@sleepysheepy.dev>
2021-10-09 09:59:59 -0400

    Fix command udeploy function

--- src/commands/utils/deploy.ts
+++ src/commands/utils/deploy.ts
@@ -3,14 +3,12 @@ import { GuildConfig } from '../../models'
 import { Commands } from '..'
 
 async function deployCommands (guildConfig: GuildConfig): Promise<void> {
-  if (process.env.NODE_ENV === 'production') {
-    await WinnieClient.client.application?.commands.set(await Commands.commandData(GuildConfig.DEFAULT_LOCALE))
-  } else {
-    const guild = await WinnieClient.client.guilds.fetch(guildConfig.id)
-    if (guild == null) { return }
+  if (!WinnieClient.isLoggedIn()) { return }
 
-    await guild.commands.set(await Commands.commandData(guildConfig.locale))
-  }
+  const guild = await WinnieClient.client.guilds.fetch(guildConfig.id)
+  if (guild == null) { return }
+
+  await guild.commands.set(await Commands.commandData(guildConfig.locale))
 }
 
 export { deployCommands }
