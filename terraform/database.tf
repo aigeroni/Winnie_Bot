18b426494921fc725b4d2542e841f3e97460c19a
Dawn E. Collett <lisushka@users.noreply.github.com>
2021-10-21 10:20:44 +1100

    Set up Infrastructure as Code and continuous deployment (#204)
    * initial cut of Winnie infra files
    
    * Configure database and firewall rules
    
    * First cut of droplet firewall rule
    
    * Mend naming; export and save connection string
    
    * Update droplet image to Debian 11
    
    * Rename droplet key to match exported name
    
    * Run release on infra branch
    
    * Remove branch restriction
    
    * Remove all restrictions
    
    * Fix droplet name
    
    * Mark database connection URI as sensitive
    
    * Add Terraform Cloud backend [skip ci]
    
    * Remove `doctl`, initialise terraform cloud
    
    * Build tfvars file as per HashiCorp standard
    
    * Use correct directory
    
    * Do I understand how to use keys correctly?
    
    * Add auto approve to terraform apply
    
    * Remove potentially superfluous quotes
    
    * Remove more superfluous quotes
    
    * Explicitly add SSH port to action
    
    * Strip double quotes from Terraform output
    
    * specify port again
    
    * Try things that will probably not work
    
    * Spoiler: it didn't work
    
    * mend dashes
    
    * Multiline??
    
    * Debug by prints
    
    * bin terraform wrapper
    
    * strip quotes
    
    * Do I know how to read?
    
    * Install Git in CI/CD
    
    * switch to apt-get for stable CLI
    
    * Add outbound firewall rules
    
    * Switch git clone to HTTPS
    
    * Update Node to 16.x in CD
    
    * Update list of installed packages
    
    * always pull
    
    * silence errors
    
    * fix Yarn install
    
    * bah, remove that other Yarn install
    
    * Why is apt always years out of date?
    
    * Add autoconfirm for Node install
    
    * add automatic flag for autoremove
    
    * We *should* no longer need autoremove
    
    * General cleanup prior to PR [skip ci]
    
    * re-up branch protection

---
+++ terraform/database.tf
@@ -0,0 +1,28 @@
+resource "digitalocean_database_cluster" "winnie-db-cluster" {
+  name       = "winnie-db"
+  engine     = "pg"
+  version    = "13"
+  size       = "db-s-1vcpu-1gb"
+  region     = "ams3"
+  node_count = 1
+}
+
+resource "digitalocean_database_db" "winnie-db" {
+  cluster_id = digitalocean_database_cluster.winnie-db-cluster.id
+  name       = "Winnie_DB"
+}
+
+resource "digitalocean_database_firewall" "winnie-db-firewall" {
+  depends_on = [digitalocean_droplet.winnie-bot]
+
+  cluster_id = digitalocean_database_cluster.winnie-db-cluster.id
+  rule {
+    type  = "droplet"
+    value = digitalocean_droplet.winnie-bot.id
+  }
+}
+
+output "database-connection-uri" {
+  value      = digitalocean_database_cluster.winnie-db-cluster.private_uri
+  sensitive  = true
+}
---
+++ terraform/droplets.tf
@@ -0,0 +1,52 @@
+data "digitalocean_ssh_key" "winnie-key" {
+  name = "winnie-key"
+}
+
+resource "digitalocean_droplet" "winnie-bot" {
+  image  = "debian-11-x64"
+  name   = "bot"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.winnie-key.id
+  ]
+}
+
+resource "digitalocean_firewall" "winnie-bot-firewall" {
+  name = "block-port-access"
+
+  droplet_ids = [digitalocean_droplet.winnie-bot.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"] // TODO: we will need to look at limiting this using the Github Actions API
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0"] // TODO: we will need to look at limiting this using the Github Actions API
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+output "droplet-ip-address" {
+  value      = digitalocean_droplet.winnie-bot.ipv4_address
+}
