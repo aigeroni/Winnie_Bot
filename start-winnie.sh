efbfb560b94c5872baa171983cf4ae31321fa0e2
Dawn E. Collett <lisushka@users.noreply.github.com>
2021-10-21 15:36:19 +1100

    Worker will start regardless of migration fail

--- start-winnie.sh
+++ start-winnie.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 
 yarn install && yarn build
-yarn typeorm migration:run && yarn start:worker:goal
+yarn typeorm migration:run 
+yarn start:worker:goal
 # yarn start:worker:challenge
 yarn start
