ad6a0d5ac64b957bce454f53e3d3912906178f1b
lisushka <lisushka@users.noreply.github.com>
2021-11-01 00:03:30 +1100

    start services

--- start-winnie.sh
+++ start-winnie.sh
@@ -10,6 +10,8 @@ cp ./services/*.service /lib/systemd/system/
 sudo systemctl enable goal-worker.service
 # sudo systemctl enable challenge-worker.service
 sudo systemctl enable app.service
+sudo systemctl start goal-worker.service
+sudo systemctl start app.service
 systemctl status goal-worker.service
 # sudo systemctl start challenge-worker.service
 systemctl status app.service
