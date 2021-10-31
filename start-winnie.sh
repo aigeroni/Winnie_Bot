6e7db69bdabbbe1bcc6208c924f229aa927b985c
lisushka <lisushka@users.noreply.github.com>
2021-10-31 23:38:21 +1100

    comment out challenge workers

--- start-winnie.sh
+++ start-winnie.sh
@@ -13,8 +13,8 @@ yarn typeorm migration:run
 cp ./services/*.service /lib/systemd/system/
 
 sudo systemctl enable goal-worker.service
-sudo systemctl enable challenge.service
+# sudo systemctl enable challenge-worker.service
 sudo systemctl enable app.service
 sudo systemctl start goal-worker.service
-sudo systemctl start challenge.service
+# sudo systemctl start challenge-worker.service
 sudo systemctl start app.service
