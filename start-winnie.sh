d9b50e55fda35161166786baf63c25a5e8a3e44c
lisushka <lisushka@users.noreply.github.com>
2021-11-01 01:16:15 +1100

    general cleanup of services

--- start-winnie.sh
+++ start-winnie.sh
@@ -7,11 +7,15 @@ yarn typeorm migration:run
 
 cp ./services/*.service /lib/systemd/system/
 
+sudo systemctl stop goal-worker.service
+# sudo systemctl stop challenge-worker.service
+sudo systemctl stop app.service
 sudo systemctl enable goal-worker.service
 # sudo systemctl enable challenge-worker.service
 sudo systemctl enable app.service
 sudo systemctl start goal-worker.service
+# sudo systemctl start challenge-worker.service
 sudo systemctl start app.service
 systemctl status goal-worker.service
-# sudo systemctl start challenge-worker.service
+# systemctl status challenge-worker.service
 systemctl status app.service
