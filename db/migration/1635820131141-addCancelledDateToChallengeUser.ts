082ade2732fa1b282b1404693975c8f98ecd8a3f
Dawn E. Collett <lisushka@users.noreply.github.com>
2021-11-02 13:41:01 +1100

    Add cancelled at to the challenge_users table

---
+++ db/migration/1635820131141-addCancelledDateToChallengeUser.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class addCancelledDateToChallengeUser implements MigrationInterface {
+
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn('challenge_user', new TableColumn({
+      name: 'cancelled_at',
+      type: 'varchar',
+      isNullable: true
+    }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('challenge_user', 'cancelled_at')
+  }
+}
