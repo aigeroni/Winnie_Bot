664146750fa54ba7ffaa6f4771f3bb3caa535f44
Katie Macke <katie@sleepysheepy.dev>
2021-11-03 15:21:03 -0400

    fix mentions

--- src/events/message.ts
+++ src/events/message.ts
@@ -13,7 +13,7 @@ import { Message, Permissions } from 'discord.js'
  */
 async function handleMention (message: Message, guildConfig: GuildConfig): Promise<void> {
   if (WinnieClient.client.user == null) { return }
-  if (!message.mentions.has(WinnieClient.client.user?.id)) { return }
+  if (!message.mentions.has(WinnieClient.client.user, { ignoreEveryone: true, ignoreRoles: true })) { return }
   Logger.info('Winnie successfully registered mention')
 
   const response = await I18n.translate(guildConfig.locale, 'mentionResponse')
