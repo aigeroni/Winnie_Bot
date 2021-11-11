7dbe241fdca867a6c175b31234939af824d2187b
lisushka <lisushka@users.noreply.github.com>
2021-11-11 20:29:09 +1100

    Spin up multiple workers of each type

--- start-winnie.sh
+++ start-winnie.sh
@@ -7,15 +7,30 @@ yarn typeorm migration:run
 
 cp ./services/*.service /lib/systemd/system/
 
-sudo systemctl stop goal-worker.service
-# sudo systemctl stop challenge-worker.service
+sudo systemctl stop goal-worker@1.service
+sudo systemctl enable goal-worker@1.service
+sudo systemctl start goal-worker@1.service
+sudo systemctl stop goal-worker@2.service
+sudo systemctl enable goal-worker@2.service
+sudo systemctl start goal-worker@2.service
+sudo systemctl stop goal-worker@3.service
+sudo systemctl enable goal-worker@3.service
+sudo systemctl start goal-worker@3.service
+# sudo systemctl stop challenge-worker@1.service
+# sudo systemctl enable challenge-worker@1.service
+# sudo systemctl start challenge-worker@1.service
+# sudo systemctl stop challenge-worker@2.service
+# sudo systemctl enable challenge-worker@2.service
+# sudo systemctl start challenge-worker@2.service
+# sudo systemctl stop challenge-worker@3.service
+# sudo systemctl enable challenge-worker@3.service
+# sudo systemctl start challenge-worker@3.service
+# sudo systemctl stop challenge-worker@4.service
+# sudo systemctl enable challenge-worker@4.service
+# sudo systemctl start challenge-worker@4.service
 sudo systemctl stop app.service
-sudo systemctl enable goal-worker.service
-# sudo systemctl enable challenge-worker.service
 sudo systemctl enable app.service
-sudo systemctl start goal-worker.service
-# sudo systemctl start challenge-worker.service
 sudo systemctl start app.service
-systemctl status goal-worker.service
-# systemctl status challenge-worker.service
+systemctl status goal-worker@1.service
+# systemctl status challenge-worker@1.service
 journalctl -u app.service
