4e5eb1cfb65c7850df232e1d2ea22a697a33a4b1
Dawn E. Collett <lisushka@users.noreply.github.com>
2021-11-14 14:01:52 +1100

    appease linter

--- src/commands/challenge/status.ts
+++ src/commands/challenge/status.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction } from 'discord.js'
-import { ChainWar, Challenge, ChallengeController, GuildConfig, Race, War } from '../../models'
+import { ChainWar, Challenge, GuildConfig, Race, War } from '../../models'
 import { ChallengeService } from '../../services'
 import { I18n, Logger } from '../../core'
 import { SubCommand } from '../../types'
@@ -25,9 +25,10 @@ export const ChallengeStatusCommand: SubCommand = {
 
   execute: async (interaction: CommandInteraction, guildConfig: GuildConfig) => {
     const challenge = await ChallengeService.getChallengeFromCommand(interaction, guildConfig)
+    const challengeId = interaction.options.getInteger('id') ?? 0
     if (challenge == null) {
       await interaction.reply(await I18n.translate(guildConfig.locale, 'commands:challenge.status.error.challengeDoesNotExist', {
-        id: interaction.options.getInteger('id') || ''
+        id: challengeId
       }))
     } else if (challenge.errors.length > 0) {
       await interaction.reply(await I18n.translate(guildConfig.locale, 'commands:challenge.status.error.couldNotGetStatus'))
@@ -40,7 +41,7 @@ export const ChallengeStatusCommand: SubCommand = {
 async function printStatus (challenge: Challenge, interaction: CommandInteraction, guildConfig: GuildConfig): Promise<void> {
   if (!challenge.isActive()) { // check whether challenge has been cancelled
     await interaction.reply(await I18n.translate(guildConfig.locale, 'commands:challenge.status.error.challengeDoesNotExist', {
-      id: interaction.options.getInteger('id') || ''
+      id: interaction.options.getInteger('id') ?? ''
     }))
   } else {
     switch (challenge.challenge_type) {
@@ -62,7 +63,7 @@ async function warStatus (war: War, interaction: CommandInteraction, guildConfig
   if (war.hasStarted) { // war is running
     await interaction.reply(await I18n.translate(guildConfig.locale, 'challenges:status.warAfterStart', {
       challengeName: war.name,
-      id: interaction.options.getInteger('id') || '',
+      id: interaction.options.getInteger('id') ?? '',
       data: await I18n.translate(guildConfig.locale, 'challenges:dataWithRemaining.war', {
         duration: await I18n.translate(guildConfig.locale, 'challenges:minutesWithCount.minutes', { count: war.duration }),
         timeLeft: await getTimeRemaining(war.startAt.plus(Duration.fromObject({ minutes: war.duration })))
@@ -128,14 +129,14 @@ async function raceStatus (race: Race, interaction: CommandInteraction, guildCon
 
 async function getTimeRemaining (eventTime: DateTime): Promise<string> {
   const timeLeft = eventTime.diff(DateTime.utc(), ['minutes', 'seconds']).toObject()
-  if (timeLeft.seconds == null) {
+  if (timeLeft.minutes == null || timeLeft.seconds == null) {
     return 'invalid time'
   } else {
     let secondsLeft = timeLeft.seconds.toFixed(0)
     if (timeLeft.seconds < 10) {
       secondsLeft = '0' + secondsLeft
     }
-    const timeString = timeLeft.minutes + ':' + secondsLeft
+    const timeString = timeLeft.minutes.toString() + ':' + secondsLeft
     return timeString
   }
 }
