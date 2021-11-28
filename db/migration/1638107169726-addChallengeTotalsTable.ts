f3ae3327ad3997bccbce8ce2aa04fe2b67d84262
lisushka <lisushka@users.noreply.github.com>
2021-11-29 00:55:03 +1100

    Add skeleton migration files

---
+++ db/migration/1638107169726-addChallengeTotalsTable.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class addChallengeTotalsTable1638107169726 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+  }
+}
