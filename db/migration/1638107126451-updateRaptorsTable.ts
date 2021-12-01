94bb690cf037c1f123c898c04b6d1e8573bec42f
lisushka <lisushka@users.noreply.github.com>
2021-12-01 22:53:01 +1100

    remove foreign keys to get migration running

--- db/migration/1638107126451-updateRaptorsTable.ts
+++ db/migration/1638107126451-updateRaptorsTable.ts
@@ -9,31 +9,31 @@ export class updateRaptorsTable1638107126451 implements MigrationInterface {
       default: '2021-11'
     }))
 
-    const raptorGuildForeignKey = new TableForeignKey({
-      columnNames: ['guild_id'],
-      referencedColumnNames: ['id'],
-      referencedTableName: 'guild_config'
-    })
-    const raptorUserForeignKey = new TableForeignKey({
-      columnNames: ['user_id'],
-      referencedColumnNames: ['id'],
-      referencedTableName: 'user_config'
-    })
+    // const raptorGuildForeignKey = new TableForeignKey({
+    //   columnNames: ['guild_id'],
+    //   referencedColumnNames: ['id'],
+    //   referencedTableName: 'guild_config'
+    // })
+    // const raptorUserForeignKey = new TableForeignKey({
+    //   columnNames: ['user_id'],
+    //   referencedColumnNames: ['id'],
+    //   referencedTableName: 'user_config'
+    // })
 
-    await queryRunner.createForeignKey('raptors', raptorGuildForeignKey)
-    await queryRunner.createForeignKey('raptors', raptorUserForeignKey)
+    // await queryRunner.createForeignKey('raptors', raptorGuildForeignKey)
+    // await queryRunner.createForeignKey('raptors', raptorUserForeignKey)
   }
 
   public async down (queryRunner: QueryRunner): Promise<void> {
-    const raptorsTable = await queryRunner.getTable('goals')
-    const raptorGuildForeignKey = raptorsTable?.foreignKeys.find(fk => fk.columnNames.includes('guild_id'))
-    if (raptorGuildForeignKey != null) {
-      await queryRunner.dropForeignKey('raptors', raptorGuildForeignKey)
-    }
-    const raptorOwnerForeignKey = raptorsTable?.foreignKeys.find(fk => fk.columnNames.includes('owner_id'))
-    if (raptorOwnerForeignKey != null) {
-      await queryRunner.dropForeignKey('raptors', raptorOwnerForeignKey)
-    }
+    // const raptorsTable = await queryRunner.getTable('goals')
+    // const raptorGuildForeignKey = raptorsTable?.foreignKeys.find(fk => fk.columnNames.includes('guild_id'))
+    // if (raptorGuildForeignKey != null) {
+    //   await queryRunner.dropForeignKey('raptors', raptorGuildForeignKey)
+    // }
+    // const raptorOwnerForeignKey = raptorsTable?.foreignKeys.find(fk => fk.columnNames.includes('owner_id'))
+    // if (raptorOwnerForeignKey != null) {
+    //   await queryRunner.dropForeignKey('raptors', raptorOwnerForeignKey)
+    // }
 
     await queryRunner.dropColumn('raptors', 'period_id')
   }
