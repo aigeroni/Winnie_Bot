d4ff9b674afeb88dc6f5548469b26cd24e06a3c2
lisushka <lisushka@users.noreply.github.com>
2021-12-28 17:04:34 +1100

    Document period config migration

--- db/migration/1638106941492-createPeriodConfigTable.ts
+++ db/migration/1638106941492-createPeriodConfigTable.ts
@@ -5,24 +5,41 @@ export class createPeriodConfigTable1638106941492 implements MigrationInterface 
     const periodConfigTable = new Table({
       name: 'period_config',
       columns: [
+        /**
+         * The ID of the period (in yyyy-mm format)
+         */
         {
           name: 'id',
           type: 'varchar',
           length: '7'
         },
+        /**
+         * The year of the period.
+         */
         {
           name: 'year',
           type: 'int'
         },
+        /**
+         * The month of the period.
+         */
         {
           name: 'month',
           type: 'int'
         },
+        /**
+         * A text string describing the period.
+         */
         {
           name: 'period_text',
           type: 'varchar',
           length: '20'
         },
+        /**
+         * A text string describing any abnormal events during the period.
+         * 
+         * This can include outages, data loss, or configuration changes.
+         */
         {
           name: 'period_note',
           type: 'varchar',
