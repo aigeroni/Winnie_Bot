531a79281e1399d995f93941172e1105e6ebcfef
lisushka <lisushka@users.noreply.github.com>
2021-12-28 17:18:22 +1100

    Document project table migration

--- db/migration/1638106953553-createProjectsTable.ts
+++ db/migration/1638106953553-createProjectsTable.ts
@@ -5,69 +5,127 @@ export class createProjectsTable1638106953553 implements MigrationInterface {
     const projectsTable = new Table({
       name: 'projects',
       columns: [
+        /**
+         * The ID of the project.
+         *
+         * Primary key, auto-incrementing.
+         */
         {
           name: 'id',
           type: 'serial',
           isPrimary: true
         },
+        /**
+         * The project's name, used in project views.
+         */
         {
           name: 'name',
           type: 'varchar',
           length: '150'
         },
+        /**
+         * The target for the project.
+         *
+         * example: 80000 words, 400 pages
+         */
         {
           name: 'target',
           type: 'int'
         },
+        /**
+         * The type of goal for which the user is aiming for this project.
+         *
+         * Can be one of pages, words, minutes, lines, or items
+         */
         {
           name: 'target_type',
           type: 'enum',
           enum: ['items', 'lines', 'minutes', 'pages', 'words']
         },
+        /**
+         * The progress towards completing the project.
+         *
+         * example: 73 out of 120 pages
+         */
         {
           name: 'progress',
           type: 'int'
         },
+        /**
+         * The current status of the mission.  Can be Created, Running (for challenges only), Completed, or Canceled.
+         */
         {
           name: 'status',
           type: 'enum',
           enum: ['created', 'canceled', 'completed']
         },
+        /**
+         * The id of the guild that the mission belongs to.
+         */
         {
           name: 'guild_id',
           type: 'varchar',
           length: '30'
         },
+        /**
+         * The id of the user that created the mission.
+         */
         {
           name: 'owner_id',
           type: 'varchar',
           length: '30'
         },
+        /**
+         * The id of the channel in which the project was created.
+         *
+         * Used for sending messages about the project's status later.
+         */
         {
           name: 'channel_id',
           type: 'varchar',
           length: '30'
         },
+        /**
+         * Timestamp of when this mission was created.
+         */
         {
           name: 'created_at',
           type: 'varchar',
           isNullable: true
         },
+        /**
+         * Timestamp of the most recent update to the mission.
+         *
+         * Null if never updated.
+         */
         {
           name: 'updated_at',
           type: 'varchar',
           isNullable: true
         },
+        /**
+         * Timestamp of when this mission was canceled.
+         *
+         * Null if not canceled.
+         */
         {
           name: 'canceled_at',
           type: 'varchar',
           isNullable: true
         },
+        /**
+         * Timestamp of when this mission was completed.
+         *
+         * Null if not completed.
+         */
         {
           name: 'completed_at',
           type: 'varchar',
           isNullable: true
         },
+        /**
+         * The due date of the project.
+         */
         {
           name: 'due_at',
           type: 'varchar',
