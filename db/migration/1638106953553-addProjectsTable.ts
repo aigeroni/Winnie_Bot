97ecc5136ed04b0600b404418cd741046176eea4
lisushka <lisushka@users.noreply.github.com>
2021-12-28 17:00:20 +1100

    Remove duped migrations

--- db/migration/1638106953553-addProjectsTable.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm'
-
-export class addProjectTable1638106953553 implements MigrationInterface {
-  public async up (queryRunner: QueryRunner): Promise<void> {
-  }
-
-  public async down (queryRunner: QueryRunner): Promise<void> {
-  }
-}
--- db/migration/1638107150209-addChallengeTable.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm'
-
-export class addChallengeTable1638107150209 implements MigrationInterface {
-  public async up (queryRunner: QueryRunner): Promise<void> {
-  }
-
-  public async down (queryRunner: QueryRunner): Promise<void> {
-  }
-}
