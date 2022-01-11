9c3e701d5f683c473a21c520bfe22a1b248ab1e0
Dawn E. Collett <lisushka@users.noreply.github.com>
2022-01-11 22:19:43 +1100

    Phase 1 of prompt files

---
+++ src/commands/prompt/index.ts
@@ -0,0 +1,30 @@
+import { ApplicationCommandData, CommandInteraction } from 'discord.js'
+import { I18n } from '../../core'
+import { GuildConfig } from '../../models'
+import { Command } from '../../types'
+import { PromptTypes } from '../../types/prompts'
+
+const NAME = 'prompt'
+
+export const PromptCommand: Command = {
+  name: NAME,
+  commandData: async (locale: string): Promise<ApplicationCommandData> => ({
+    name: NAME,
+    description: await I18n.translate(locale, 'commands:prompt.description'),
+    options: [
+      {
+        name: 'type',
+        description: await I18n.translate(locale, 'commands:prompt.args.type'),
+        type: 'STRING',
+        choices: Object.values(PromptTypes).map((type) => ({
+          name: type,
+          value: type
+        })),
+        required: false
+      }
+    ]
+  }),
+  execute: async (interaction: CommandInteraction, guildConfig: GuildConfig) => {
+    true
+  }
+}
