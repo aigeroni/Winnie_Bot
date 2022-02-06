aca2f10718eb0cae665d2752ac28617c4ef4cf4f
Dawn E. Collett <lisushka@users.noreply.github.com>
2022-02-06 11:19:31 +1100

    remove superfluous reference to user config

--- src/commands/goal/set.ts
+++ src/commands/goal/set.ts
@@ -43,7 +43,7 @@ export const GoalSetCommand: SubCommand = {
     ]
   }),
   execute: async (interaction: CommandInteraction, guildConfig: GuildConfig) => {
-    const userConfig = await UserConfig.findOrCreate(interaction.user.id)
+    await UserConfig.findOrCreate(interaction.user.id)
     if (await userHasActiveGoal(interaction, guildConfig.locale)) { return }
 
     const goalTimezone = await userTimezone(interaction, guildConfig)
