d1bb588de594d50d5d89c0f16fba265bcf293ab9
Dawn E. Collett <lisushka@users.noreply.github.com>
2022-08-07 13:04:10 +1000

    Add prompts (#241)
    * Phase 1 of prompt files
    
    * Fix linting issues [skip ci]
    
    * Add skeleton files for prompts
    
    * Build prompt command
    
    * Add prompt data to command locale files
    
    * Add more prompts for character/world

---
+++ src/types/prompts.ts
@@ -0,0 +1,24 @@
+/**
+ * All possible prompt types.
+ */
+export enum PromptTypes {
+  ADVENTURE = 'adventure',
+  CHARACTER = 'character',
+  CHILDREN = 'children',
+  COMEDY = 'comedy',
+  CONTEMPORARY = 'contemporary',
+  FANTASY = 'fantasy',
+  FOLK = 'folk',
+  HISTORICAL = 'historical',
+  HORROR = 'horror',
+  LITERARY = 'literary',
+  LGBTQ = 'lgbtq',
+  MYSTERY = 'mystery',
+  PLOT = 'plot',
+  ROMANCE = 'romance',
+  SCIFI = 'scifi',
+  STORY = 'story',
+  SUPERNATURAL = 'supernatural',
+  THRILLER = 'thriller',
+  WORLD = 'worldbuilding'
+}
